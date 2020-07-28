5adb546d16d0c4e498a6e5798baa1cba44353c66
Arthur Pinheiro <arthurpinheirodelima@gmail.com>
2020-07-28 17:45:50 -0300

    Exemplo de uma aplicacao produtor/consumidor com kafka

---
+++ src/main/java/com/demo/kafka/springbootwithkafka/models/User.java
@@ -0,0 +1,12 @@
+package com.demo.kafka.springbootwithkafka.models;
+
+public class User {
+
+    private String name;
+    private String age;
+
+    public User(String name, String age) {
+        this.name = name;
+        this.age = age;
+    }
+}
